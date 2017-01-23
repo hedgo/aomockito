b420d350552a244ca41c5726663560c23ffb1105
Andrzej Okon <andrzej.okon@pekao.com.pl>
2017-01-23 15:57:26 +0100

    3 commit

---
+++ src/test/java/com/hedgo/mockito/test/ProductService/AllTests.java
@@ -0,0 +1,13 @@
+package com.hedgo.mockito.test.ProductService;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ProductServiceTest.class, ProductServiceTest2.class })
+public class AllTests {
+
+	//Ctrl+N->JAVA->Junit->Junit Test Suit      - tak siê robi automatycznie t¹ klasê!
+	
+}
---
+++ src/test/java/com/hedgo/mockito/test/ProductService/ProductServiceTest2.java
@@ -0,0 +1,80 @@
+package com.hedgo.mockito.test.ProductService;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*; //tu mam np. when lub stub:)
+
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.hedgo.mockito.business.ProductService;
+import com.hedgo.mockito.data.ClientDAO;
+import com.hedgo.mockito.model.Product;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProductServiceTest2 {
+		
+	@Mock
+	ClientDAO clientDAO;
+		
+	@InjectMocks
+	ProductService productService = new ProductService();
+
+	@Before
+	public void setUp() {
+		//MockitoAnnotations.initMocks(this);
+		//when(.....)
+	}
+	
+	@Test
+	public void testSumBasket()
+	{
+		//given
+		//tu mo¿na inicjalizowaæ obiekty do tego testu
+        when(clientDAO.getAllProducts()).thenReturn(getAllProducts());			//mo¿na te¿ tak: stub(clientDAO.getAllProducts()).toReturn(getAllProducts());
+		
+		//when
+        int result = productService.sumBasket();
+		
+        //then
+		assertEquals(6,result);
+				
+		//then
+		//Assertions.assertThat(result).hasSize(1);
+		//Assertions.assertThat(result.get(0).getInterestRate()).isEqualByComparingTo(BigDecimalUtility.bd("0.01"));
+		//assertNotNull
+		//assertFalse
+		//assertTrue
+        //assertThat(actual, is(user));
+        //assertThat(user.getCreatedAt(), is(notNullValue()));
+        //assertThat(user.getUpdatedAt(), is(notNullValue()));
+        //assertThat(user.getPassword(), is("xxxx"));		
+	}
+
+	@Test
+	public void testSumBasket2()
+	{
+        when(clientDAO.getAllProducts()).thenReturn(getAllProducts());			//mo¿na te¿ tak: stub(clientDAO.getAllProducts()).toReturn(getAllProducts());
+        int result = productService.sumBasket();
+		assertEquals(6,result);
+	}	
+	
+	private List<Product> getAllProducts()
+	{
+		List<Product> myProducts;
+		myProducts = new ArrayList<Product>();
+		myProducts.add(new Product(1,"eurokonto"));
+		myProducts.add(new Product(2,"oszczêdnoœciowe"));
+		myProducts.add(new Product(3,"walutowe"));
+		return myProducts;
+	}
+	
+}
